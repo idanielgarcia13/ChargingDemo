d3e8d3ae733062a00a05e27ba2f460c3444e303a
Daniel Garcia <daniel_garcia@graycor.com>
2021-02-04 22:01:02 -0600

    Initial Commit

---
+++ ChargingDemo/ChargingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargingDemoApp.swift
+//  ChargingDemo
+//
+//  Created by Daniel Garcia on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ChargingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
